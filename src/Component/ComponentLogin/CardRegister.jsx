717b6a151855e2ee4da765d2ce46486c5b734f3e
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-21 14:30:17 +0700

    selesai refactoring component Login

---
+++ src/Component/ComponentLogin/CardRegister.jsx
@@ -0,0 +1,36 @@
+
+
+
+const CardRegister = ()=>{
+    return (
+            <div className="card-register w-full min-[420px]:w-[400px]  p-4 rounded-md">
+                <h3 className="title-register text-2xl text-white">Register</h3>
+              
+                <form action="" className="relative  mt-4" autoComplete="no">
+                    <div className="input-group mt-2">
+                            <input type="text" className="input-item" placeholder="Firstname..." />
+                    </div>
+                    <div className="input-group  mt-2">
+                            <input type="text" className="input-item" placeholder="Lastname..." />
+                    </div>
+                    <div className="input-group mt-2">
+                            <input type="email" className="input-item" placeholder="email..." />
+                    </div>
+
+                    <div className="input-group  mt-2">
+                            <input type="password" className="input-item" placeholder="password..." />
+                    </div>
+                    <div className="input-group  mt-2">
+                            <input type="password" className="input-item" placeholder="Confirm password..." />
+                    </div>
+                    <button className="btn btn-login border-2" type="submit">
+                        Register
+                    </button>
+                </form>
+                <span className="block text-center text-[0.8em] text-white mt-1">Have account ?</span>
+            </div> 
+
+    )
+}
+
+export default CardRegister;
