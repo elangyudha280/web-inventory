4c4c1582689303f2cfadfec9a12970bb0e3ad0c1
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-22 23:52:51 +0700

    mulai desain component card itme book

--- src/Component/ComponentDashboard/ContainerHomeDashboard.jsx
+++ src/Component/ComponentDashboard/ContainerHomeDashboard.jsx
@@ -6,8 +6,14 @@ import BookInfo from "./ComponentHomeDashboard/BookInfo";
 const ContainerHomeDashboard = ()=>{
     return (
         <section className="container-home  pt-[100px] px-4 pb-5 overflow-x-hidden overflow-y-auto z-[1] relative w-full h-[100vh] border-2 border-red-500">
-            <section className="sub-container relative max-w-6xl ">
+            <section className="sub-container mx-auto relative max-w-6xl ">
                   <BookInfo/>
+
+                {/* component card book item */}
+                <div className="container-card-book-item mt-10 relative w-full border-2 border-red-600">
+                    
+
+                </div>
             </section>
         </section>
     )
