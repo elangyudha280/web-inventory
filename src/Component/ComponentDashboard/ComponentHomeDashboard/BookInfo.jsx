12e5c21e9c9f111b728f0050ff45179917c356f1
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-02-23 00:12:15 +0700

    selesai nav book item

--- src/Component/ComponentDashboard/ComponentHomeDashboard/BookInfo.jsx
+++ src/Component/ComponentDashboard/ComponentHomeDashboard/BookInfo.jsx
@@ -4,7 +4,7 @@
 
 const BookInfo = ()=>{
     return (
-        <div className="container-book-info  relative w-full grid  gap-2  sm:grid-cols-[1fr_1fr]   auto-rows-auto place-items-center lg:grid-cols-[1fr_1fr_1fr]">
+        <div className="container-book-info   relative w-full grid  gap-2  sm:grid-cols-[1fr_1fr]   auto-rows-auto place-items-center lg:grid-cols-[1fr_1fr_1fr]">
         <div className="card-info-book w-full  py-5 px-3 flex-1 border-2 rounded-xl bg-slate-800 overflow-hidden ">
             <h2 className="title-card-info-book text-white text-center text-xl ">Jumlah Product</h2>
             <h2 className="count-jumlah-product text-white text-center text-xl mt-5 truncate">
--- src/Component/ComponentDashboard/ContainerHomeDashboard.jsx
+++ src/Component/ComponentDashboard/ContainerHomeDashboard.jsx
@@ -10,8 +10,20 @@ const ContainerHomeDashboard = ()=>{
                   <BookInfo/>
 
                 {/* component card book item */}
-                <div className="container-card-book-item mt-10 relative w-full border-2 border-red-600">
-                    
+                <div className="container-card-book-item mt-10 relative w-full ">
+                    <nav className="nav-card-book-item relative w-full flex  gap-3 h-auto max-[500px]:flex-col min-[500px]:h-[40px]">
+                        <button className="btn-add-data-book border-none outline-none px-3 rounded-xl bg-blue-600 text-white transition-all duration-200 hover:opacity-70 h-[50px] min-[500px]:h-full">tambah buku</button>
+                        <div className="flex-1 flex gap-3 justify-end">
+                            <form action="">
+                                <input type="text" className="input-search-buku text-black placeholder:text-black w-full min-[500px]:w-[150px] bg-transparent border-b-[1px] border-slate-800 outline-none text-sm h-full px-2" placeholder="cari buku..."  />
+                            </form>
+                            <select name="" id="" className="px-2 h-[40px] min-[500px]:h-full bg-blue-700 rounded-lg text-white" >
+                                <option>filter buku</option>
+                                <option value="buku_fiksi" className="truncate">bukufiksi</option>
+
+                            </select>
+                        </div>
+                    </nav>
 
                 </div>
             </section>
